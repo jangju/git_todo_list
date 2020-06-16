9d7b48e52029aca100e78b1d1c30c2317d28906e
jangju <wkdwndud13@naver.com>
2020-06-17 02:26:07 +0900

    todo_list2

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,66 +1,52 @@
 <template>
-  <div>
-      <ul>
-        
-        <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
-          <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" 
-          v-on:click="toggleComplete(todoItem, index)"></i>
-          <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
-          <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
-            <i class="fas fa-trash-alt"></i>
-          </span>
-        </li>
-      </ul>
-  </div>
+  <section>
+    <ul>
+      <li v-for="(todoItem, index) in propsdata" class="shadow" v-bind:key="todoItem.item">
+        <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
+        <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="removeBtn fas fa-trash-alt"></i>
+        </span>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {
   props: ['propsdata'],
-  
-  methods:{
-    removeTodo: function(todoItem, index){
-      console.log(todoItem, index);
-      localStorage.removeItem(todoItem);
-      this.todoItems.splice(index, 1); //splice 기존있는 배열을 지움
-      
+  methods: {
+    removeTodo: function(todoItem, index) {
+      this.$emit('removeItem', todoItem, index);
     },
-  toggleComplete: function(todoItem, index){
-    todoItem.completed = !todoItem.completed;
-    localStorage.removeItem(todoItem.item);
-    localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
-    console.log(index);
-    
-  }   
+    toggleComplete: function(todoItem, index) {
+      this.$emit('toggleItem', todoItem, index); 
+    }
   }
-
 }
 </script>
 
 <style scoped>
 ul {
   list-style-type: none;
-  padding-left: 0;
+  padding-left: 0px;
   margin-top: 0;
   text-align: left;
 }
 li {
   display: flex;
   min-height: 50px;
+  height: 50px;
   line-height: 50px;
   margin: 0.5rem 0;
   padding: 0 0.9rem;
-  border-radius: 5px;
   background: white;
+  border-radius: 5px;
 }
 .checkBtn {
   line-height: 45px;
   color: #62acde;
   margin-right: 5px;
-}
-.removeBtn {
-  margin-left: auto;
-  color: #de4343;
 }
 .checkBtnCompleted {
   color: #b3adad;
@@ -69,5 +55,8 @@ li {
   text-decoration: line-through;
   color: #b3adad;
 }
-
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
 </style>
