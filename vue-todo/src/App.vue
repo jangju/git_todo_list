9278af739d26c81a28e17fe045b03ec0f75e4397
jangju <wkdwndud13@naver.com>
2020-06-17 00:27:05 +0900

    todolist-repactoring-App.vue

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
-    <TodoList v-bind:propsdata="todoItems" v-on:removeItem=""></TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoInput v-on:addItem="addOneItem"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem" v-on:toggleItem="toggleOneItem"></TodoList>
+    <TodoFooter v-on:clearAll="clearAllItems"></TodoFooter>
   </div>
 </template>
 
@@ -12,51 +12,63 @@ import TodoHeader from './components/TodoHeader.vue'
 import TodoInput from './components/TodoInput.vue'
 import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
-
 export default {
-  data: function(){
-    return{
-      todoItems:[]
+  data: function() {
+    return {
+      todoItems: []
     }
   },
   methods: {
-    addOneItem: function(todoItem){
-    var obj = {completed: false, item: todoItem}
-    localStorage.setItem(this.newTodoItem,JSON.stringify(obj));
-    this.todoItems.push(obj);
+    addOneItem: function(todoItem) {
+      var obj = {completed: false, item: todoItem};
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      this.todoItems.push(obj);
+    },
+    removeOneItem: function(todoItem, index) {
+      this.todoItems.splice(index, 1);
+      localStorage.removeItem(todoItem.item);
+    },
+    toggleOneItem: function(todoItem, index) {
+      todoItem.completed = !todoItem.completed;
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+    clearAllItems: function() {
+      this.todoItems = [];
+      localStorage.clear();
     }
   },
-  created: function(){
-    if(localStorage.length > 0){
-      for(var i = 0; i< localStorage.length; ++i){
-        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+  created: function() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
           this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
         }
       }
     }
   },
-  components:{
-    'TodoHeader':TodoHeader,
-    'TodoInput':TodoInput,
-    'TodoList':TodoList,
-    'TodoFooter':TodoFooter
-  }
+  components: {
+    TodoHeader: TodoHeader,
+    TodoInput: TodoInput,
+    TodoList: TodoList,
+    TodoFooter: TodoFooter
+  }  
 }
 </script>
 
 <style>
-body{
+body {
   text-align: center;
-  background-color: #F6F6F6;
+  background-color: #F6F6F8;
 }
-input{
+input {
   border-style: groove;
   width: 200px;
 }
-button{
+button {
   border-style: groove;
 }
-.shadow{
-  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
 }
 </style>
